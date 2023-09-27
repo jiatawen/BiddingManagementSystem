e3150f694125def3617cc55d5ba3c104fc68ad83
jiatawen <lchenjianwenh@163.com>
2023-09-27 09:23:52 +0800

    Spring boot

---
+++ src/main/java/com/example/biddingmanagement/BiddingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.biddingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiddingManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/biddingmanagement/BiddingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biddingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
