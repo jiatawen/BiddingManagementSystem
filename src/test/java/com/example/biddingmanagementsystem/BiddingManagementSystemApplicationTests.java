400df6ff7e37bbbab650a441882a737f9d6b94d8
jiatawen <lchenjianwenh@163.com>
2023-10-09 22:46:29 +0800

    第一次完美运行

---
+++ src/test/java/com/example/biddingmanagementsystem/BiddingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biddingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
